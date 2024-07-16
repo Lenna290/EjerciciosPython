ef525dfb09b207bc5a4237655efa0d05bb300102
Lenna <danna.coello@tecnologicoliceocristiano.edu.ec>
2024-07-15 20:22:13 -0500

    Ejercicios en clases #2

---
+++ serie.py
@@ -0,0 +1,8 @@
+n = 0
+a = 1
+c =0
+for c in range(50):
+    print(n)
+    s = n + a
+    n = a
+    a = s
---
+++ serieFibonaccy.py
@@ -0,0 +1,8 @@
+a = 0
+b = 1 
+for _ in range(50):
+         print(a) 
+         r = a + b
+         a = b
+         b = r 
+         
